592c1f6d4b56fd6093f3d95aff5bc6f4ee22bd2f
Viktor Weindlmayr <viktor.weindlmayr@digitalsunray.com>
2022-03-12 14:35:58 +0100

    base structure

--- src/main/java/Flipper.java
+++
@@ -1,2 +0,0 @@
-public class Flipper implements state{
-}
---
+++ src/main/java/state/FlipperState.java
@@ -0,0 +1,18 @@
+package state;
+
+public interface FlipperState {
+
+    /*
+    NoCredit-State
+    Ready-State
+    Playing-State
+     */
+
+    void insertCoin();
+
+    void pressStartButton();
+
+    void showAuthors();
+
+
+}
